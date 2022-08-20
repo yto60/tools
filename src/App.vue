c130787c0df1ceb98fd8bc3feff1bba66c327eff
yto60 <mail@yto60.dev>
2022-08-20 14:48:02 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Md2Sb from './components/Md2Sb.vue'
+</script>
+
+<template>
+  <Md2Sb />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Md2Sb.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const mdText = ref('')
+const sbText = ref('')
+const convertText = () => {
+  // TODO
+  sbText.value = mdText.value.toUpperCase()
+}
+</script>
+
+<template>
+  <div class="grid grid-flow-col">
+    <div class="mx-4">
+      <div>Markdown</div>
+      <textarea name="md" v-model="mdText" class="border border-light-900 h-96 w-96"></textarea>
+    </div>
+
+    <button @click="convertText" class="h-fit my-auto">convert</button>
+
+    <div class="mx-4">
+      <div>Scrapbox</div>
+      <textarea name="sb" v-model="sbText" class="border border-light-900 h-96 w-96"></textarea>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
