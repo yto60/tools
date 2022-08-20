a7340f4f49a3436b55d2663b6dd5679d3563c0fe
yto60 <mail@yto60.dev>
2022-08-20 15:03:00 +0900

    できた

--- src/components/Md2Sb.vue
+++ src/components/Md2Sb.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import md2sb from 'md2sb'
+
 const mdText = ref('')
 const sbText = ref('')
-const convertText = () => {
-  // TODO
-  sbText.value = mdText.value.toUpperCase()
+const convertText = async () => {
+  sbText.value = await md2sb(mdText.value)
 }
 </script>
 
@@ -15,11 +16,11 @@ const convertText = () => {
       <textarea name="md" v-model="mdText" class="border border-light-900 h-96 w-96"></textarea>
     </div>
 
-    <button @click="convertText" class="h-fit my-auto">convert</button>
+    <button @click="convertText" class="h-fit my-auto">→</button>
 
     <div class="mx-4">
       <div>Scrapbox</div>
-      <textarea name="sb" v-model="sbText" class="border border-light-900 h-96 w-96"></textarea>
+      <textarea name="sb" v-model="sbText" class="border border-light-900 h-96 w-96" readonly></textarea>
     </div>
   </div>
 </template>
